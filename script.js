ef158a3b8478c03e7e34f8a5c3f4c318c0c784e4
Hieu Dan <hieu_danminh@live.com>
2023-11-27 14:39:36 +0100

    Final Project Push

--- script.js
+++ script.js
@@ -1,48 +1,78 @@
-/* Userinput is gernerated and logged into userInput */
-
-let userInput = prompt("Enter your Choice!").toLocaleLowerCase();
-
-console.log("User chose:" + userInput);
-
-/* Computerchoice is generated and logged into ComputerFinalChoice */
-
-let computerChoice = function getComputerChoice() {
-    let computerChoice = ["Rock", "Paper", "Scissor"];
-
-    let randomNumber = Math.floor(Math.random() * 3)
-
-    return computerChoice[randomNumber].toLowerCase();
-}
-
-let ComputerFinalChoice = computerChoice();
-
-console.log("Computer chose:"+ ComputerFinalChoice);
 
 /* Main Function */
 
+let computerPoints = 0;
+let playerPoints = 0;
+
 function comparingFunction(computerChoice, userInput) {
+
     if (computerChoice == "rock" && userInput == "scissor") {
         console.log("Computer Wins!" + computerChoice + "wins against" + userInput)
+        computerPoints++;
     } else if (computerChoice == "rock" && userInput == "rock") {
         console.log("Its a tie")
     } else if (computerChoice == "rock" && userInput == "paper") {
         console.log("Computer loses!" + computerChoice + "loses against" + userInput)
+        playerPoints++;
     } else if (computerChoice == "scissor" && userInput == "rock") {
         console.log("Computer loses!" + computerChoice + "loses against" + userInput)
+        playerPoints++;
     } else if (computerChoice == "scissor" && userInput == "paper") {
         console.log("Computer wins!" + computerChoice + "wins against" + userInput)
+        computerPoints++;
     } else if (computerChoice == "scissor" && userInput == "scissor") {
         console.log("Its a tie!")
     } else if (computerChoice == "paper" && userInput == "scissor") {
         console.log("Computer loses!" + computerChoice + "loses against" + userInput)
+        playerPoints++;
     } else if (computerChoice == "paper" && userInput == "paper") {
         console.log("Its a tie")
     } else if (computerChoice == "paper" && userInput == "rock") {
         console.log("Computer wins! " + computerChoice + "wins against " + userInput)
+        computerPoints++;
     }
     else {
         console.log("What did you chose? Choose Paper, Scissor or Rock!")
     }
+
+
 }
 
-comparingFunction(ComputerFinalChoice, userInput);
+
+function game() {
+
+    for (let index = 0; index <= 5; index++) {
+
+        /* Userinput is gernerated and logged into userInput */
+
+        let userInput = prompt("Enter your Choice!").toLocaleLowerCase();
+
+        console.log("User chose:" + userInput);
+
+        /* Computerchoice is generated and logged into ComputerFinalChoice */
+
+        let computerChoice = function getComputerChoice() {
+            let computerChoice = ["Rock", "Paper", "Scissor"];
+
+            let randomNumber = Math.floor(Math.random() * 3)
+
+            return computerChoice[randomNumber].toLowerCase();
+        }
+
+        let ComputerFinalChoice = computerChoice();
+
+        console.log("Computer chose: " + ComputerFinalChoice);
+
+        console.log(comparingFunction(ComputerFinalChoice, userInput));
+
+        console.log("Current score - Player: " + playerPoints + ", Computer: " + computerPoints);
+    }
+
+}
+
+
+game();
+
+
+
+
