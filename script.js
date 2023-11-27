93a84778cb78b602b6547da3fd407e08a39e0357
Hieu Dan <hieu_danminh@live.com>
2023-11-27 13:34:32 +0100

    updates JS File to function on all occurencies

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
+/* Userinput is gernerated and logged into userInput */
+
 let userInput = prompt("Enter your Choice!").toLocaleLowerCase();
 
-console.log(userInput);
+console.log("User chose:" + userInput);
 
+/* Computerchoice is generated and logged into ComputerFinalChoice */
 
 let computerChoice = function getComputerChoice() {
-    let computerChoice = ["Rock", "Paper", "Scissors"];
+    let computerChoice = ["Rock", "Paper", "Scissor"];
 
     let randomNumber = Math.floor(Math.random() * 3)
 
     return computerChoice[randomNumber].toLowerCase();
 }
-computerChoice();
 
-console.log(computerChoice);
+let ComputerFinalChoice = computerChoice();
+
+console.log("Computer chose:"+ ComputerFinalChoice);
+
+/* Main Function */
 
 function comparingFunction(computerChoice, userInput) {
     if (computerChoice == "rock" && userInput == "scissor") {
@@ -21,8 +27,22 @@ function comparingFunction(computerChoice, userInput) {
         console.log("Its a tie")
     } else if (computerChoice == "rock" && userInput == "paper") {
         console.log("Computer loses!" + computerChoice + "loses against" + userInput)
+    } else if (computerChoice == "scissor" && userInput == "rock") {
+        console.log("Computer loses!" + computerChoice + "loses against" + userInput)
+    } else if (computerChoice == "scissor" && userInput == "paper") {
+        console.log("Computer wins!" + computerChoice + "wins against" + userInput)
+    } else if (computerChoice == "scissor" && userInput == "scissor") {
+        console.log("Its a tie!")
+    } else if (computerChoice == "paper" && userInput == "scissor") {
+        console.log("Computer loses!" + computerChoice + "loses against" + userInput)
+    } else if (computerChoice == "paper" && userInput == "paper") {
+        console.log("Its a tie")
+    } else if (computerChoice == "paper" && userInput == "rock") {
+        console.log("Computer wins! " + computerChoice + "wins against " + userInput)
     }
     else {
         console.log("What did you chose? Choose Paper, Scissor or Rock!")
     }
 }
+
+comparingFunction(ComputerFinalChoice, userInput);
